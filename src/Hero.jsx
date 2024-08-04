0231f19ad0a1f79bfde56b9bd4cc003001632747
xXxgamechangerxXx <138011822+thanapoom404420@users.noreply.github.com>
2024-08-04 16:31:04 +0700

    Update Hero.jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,7 +3,7 @@ import heroimg from '/public/images/desktop/image-interactive.jpg'
 
 function Hero() {
   return (
-    <div className='w-full max-h-[70vh] overflow-hidden md:max-h-[100vh] sm:h-[100vh] mt-[80px] '>
+    <div className='w-full max-h-[70vh] overflow-hidden md:max-h-[100vh] sm:h-[100vh] mt-[150px] md:mt-[80px] '>
         <div className='lg:h-full lg:max-w-[1290px] lg:flex block lg:mx-auto lg:justify-center lg:items-center md:w-full md:text-center sm:text-center sm:w-full'>
             <img src={heroimg} className='lg:flex lg:h-[900px] lg:absolute lg:max-w-[1200px] md:w-full md:p-[100px] sm:w-full  p-[20px] lg:ml-[50px] xl:max-w-[1200px]'/>
             <div className='bg-white lg:max-w-[600px] lg:h-[450px] lg:absolute lg:p-[70px] xl:ml-[800px] lg:mt-[400px] lg:ml-[600px] p-[50px] text-center lg:text-left'>
